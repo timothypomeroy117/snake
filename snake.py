e17f1d7bb02ed243dcada6c71c9283aa082c386b
timothypomeroy117 <150109892+timothypomeroy117@users.noreply.github.com>
2023-11-14 23:46:15 -0700

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,21 @@
+print ("Hello World")
+
+var1 = "none"
+Var2 = 2
+
+if var1 is "none":
+  print ("It is a none variable")
+else:
+    print ("It is not a none variable")
+
+occupation = ["Doctor", "Engineer", "Analyst"]
+for x in occupation:
+    print(x) 
+
+sarah = bob = mike = 17
+print (sarah)
+print (bob)
+
+name, age = "avi" , 22
+print (age)
+print (name)
